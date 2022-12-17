49b336a89b8a260c4bdc8a3c145279cb087f2303
Igor O <94803838+kixelo@users.noreply.github.com>
2022-12-17 20:57:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QPushButton, QFileDialog
+from PyQt6.QtCore import Qt
+from pathlib import Path
+
+def open_files():
+    global filenames
+    filenames, _ = QFileDialog.getOpenFileNames(window, 'Open files')
+    message.setText('\n'.join(filenames))
+
+def destroy_files():
+    for filename in filenames:
+        path=Path(filename)
+        with open(path, "wb") as file:
+            file.write(b'')
+        path.unlink()
+    message.setText('Destruction Successfull!')
+
+app= QApplication([])
+window=QWidget()
+window.setWindowTitle('File Destroyer')
+layout=QVBoxLayout()
+
+description=QLabel('Select the files you want to destroy. The files will be <font color="red">permanently</font> deleted')
+layout.addWidget(description, alignment=Qt.AlignmentFlag.AlignCenter)
+
+open_btn=QPushButton('Open Files')
+open_btn.setToolTip('Open file')
+open_btn.setFixedWidth(100)
+layout.addWidget(open_btn, alignment=Qt.AlignmentFlag.AlignCenter )
+
+open_btn.clicked.connect(open_files)
+
+destroy_btn=QPushButton('Destroy Files')
+destroy_btn.setToolTip('Destroy File')
+destroy_btn.setFixedWidth(100)
+layout.addWidget(destroy_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+destroy_btn.clicked.connect(destroy_files)
+
+message=QLabel('')
+layout.addWidget(message, alignment=Qt.AlignmentFlag.AlignCenter)
+
+window.setLayout(layout)
+window.show()
+app.exec()
